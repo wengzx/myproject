73ae4a2725c2f4f0a8b5c8a04b021a0873cdf0c0
wuxin <wx19318195309@163.com>
2024-03-11 17:22:19 +0800

    曾加定时任务

--- streamtest.py
+++ streamtest.py
@@ -11,7 +11,9 @@ import openpyxl
 import pandas as pd
 
 import base64
+import schedule   #定时任务
 
+import yagmail
 
 
 # 设置页面标题
@@ -19,16 +21,19 @@ st.set_page_config(
         page_title="自动化接口测试平台",
 )
 
-
+# 定义多页面
 class MultiApp:
+
     def __init__(self):
         self.apps = []
         self.app_dict = {}
+
 
     def add_app(self, title, func):
         if title not in self.apps:
             self.apps.append(title)
             self.app_dict[title] = func
+
 
     def run(self):
         title = st.sidebar.radio(
@@ -39,8 +44,10 @@ class MultiApp:
         self.app_dict[title]()
 
 
-
+# api测试方法
 class apitest():
+
+    # 写入excel文件结果
 
     def write_result(self,filename,sheetname,row,column,final_result):
 
@@ -49,12 +56,16 @@ class apitest():
         sheet.cell(row=row,column=column).value=final_result #将结果写入cell
         wb.save(filename) #保存文件
 
-    def read_data(self,filename,sheetname):
+
+    # 读取excel文件
+
+    def read_data(_self,filename,sheetname):
         wb=openpyxl.load_workbook(filename) #加载工作薄
         sheet=wb[sheetname] #获取表单
         max_row=sheet.max_row #获取最大行数
         case_list=[] #创建一个空列表，存放测试用例
 
+        # 循环
         for i in range(2,max_row+1):
             dict1=dict(
             case_id=sheet.cell(row=i,column=1).value, #获取case_id
@@ -69,19 +80,23 @@ class apitest():
             case_list.append(dict1)  #每循环一次就将一行数据加入列表
         return case_list  #返回测试用例列表
 
-    def duanyan(self,response,msg,case_id,url):
-            bb={}
-            print(type(response))
-            if type(response)!=dict:
 
-                res = response.json()
+    # 断言结果是否与excel预期结果一致
+
+    def duanyan(self,response,msg,case_id,url):
+            bb={}  #定义一个空字典
+            print(type(response))
+
+            if type(response)!=dict:  #如果返回数据类型不是字典
+
+                res = response.json()  #json返回数据，将其json化成python可读数据
             else:
                 print(55)
             # print(res)
                 res=response
 
                 print(res)
-                real_msg=res.get("msg")
+                real_msg=res.get("msg")   #获取返回数据的msg
                 print(real_msg)
                 # sys.exit()
 
@@ -89,12 +104,12 @@ class apitest():
                 print("实际结果{}".format((real_msg)))
 
 
-                if real_msg==msg:
+                if real_msg==msg:             #如果返回msg和预期结果msg一致
                     print("第{}条用例通过".format(case_id))
                     final_re="passed"
                     mm="{}条".format(case_id)+"{}接口请求成功".format(url)
 
-                else:
+                else:                      ##如果返回msg和预期结果msg一致
                     print("第{}条用例不通过".format(case_id))
                     final_re="failed"
                     mm = "{}条".format(case_id)+"{}接口请求失败".format(url)+real_msg
@@ -103,11 +118,14 @@ class apitest():
                 bb['mm']=mm
                 return bb
 
-    def testreport(self,databiao):
-        biao={"main":"main_api.xlsx","test":"test_api.xlsx","pay":"pay_api.xlsx"}
-        dataxlsx=biao[databiao]
+
+    # 生成测试报告
+
+    def testreport(self,databiao):   #传入databiao
+        biao={"main":"main_api.xlsx","test":"test_api.xlsx","pay":"pay_api.xlsx"}   #将文件用字典的键值存储起来
+        dataxlsx=biao[databiao]    #获取excel文件名
         print(dataxlsx)
-        now=int(time.time())
+        now=int(time.time())     #获取时间
         data = open(dataxlsx, 'rb').read()  # 以只读模式读取且读取为二进制文件
 
         b64 = base64.b64encode(data).decode('UTF-8')  # 解码并加密为base64
@@ -116,41 +134,38 @@ class apitest():
 
 
     # 测试所有接口
+
     def api_test(self,cases): #传入读取到的测试用例数据
-        self.token=''
+        self.token=''       #定义token
         # print(cases)
         kk=[]
-        for case in cases:
+        for case in cases:     #循环每一行excel数据在读取的excel文件内容
             err=""
-            if case['case_id'] is None :
+            if case['case_id'] is None :     #如果excel的case_id列内容为空
                 baogao="表格第{}条".format(case_id+1) + "内容为空,请删除此行"
                 print(baogao)
                 continue
             print(case)
             print(type(case))
 
-            name=case.get("name")
+            name=case.get("name") #从列表中取出name
             case_id=case.get("case_id") #从列表中取出case_id
             url=case.get("url")#从列表中取出url
             method=case.get("method")#从列表中取出method
             headers=eval(case.get("headers"))#从列表中取出headers
             print(headers)
-            if 'token' in headers :
+            if 'token' in headers :    #如果请求头包含token
                 print(2223)
-                headers['token']=self.token
+                headers['token']=self.token  #将全局变量token赋值请求头的token
             else:
                pass
-            # sys.exit()
-            # return headers
+
             print("data数据")
             print(type(case["data"]))
             print(case["data"])
-            # print(case.get("data"))
-            # for da in data:
-            #     print(da)
 
             try:
-                data=eval(case.get("data"))
+                data=eval(case.get("data"))   #获取data的参数的值
             except Exception as e:
                 print(e)
                 err="请检查data参数里面是否有值未加双引号"
@@ -165,26 +180,23 @@ class apitest():
             msg=response.get("msg")#从response中取出msg
             print(case['case_id'])
 
-
-
-
             if method=="get": #判断请求方式
                 response=requests.get(url=url,headers=headers,data=data)
 
             else:   #这是post请求
                 res=requests.post(url=url,headers=headers,json=data,verify=False)#调用请求方法，传入从列表取出的url，data,hedaers
-                response = res.json()
+                response = res.json()   #返回数据json化成python可读的数据
                 aa=type(response)
                 print(aa)
-                # print(rs)
-                # rsdata=rs['data']
+
+
                 rstoken=response["data"]
                 print(rstoken)
-                if 'token' in rstoken:
+                if 'token' in rstoken:      #如果返回数据有token
                     print(rstoken['token'])
 
                     tokens=rstoken['token']
-                    self.token = tokens
+                    self.token = tokens      #将token的值赋值给全职变量self.token
                     print(self.token)
 
 
@@ -193,88 +205,68 @@ class apitest():
             self.write_result("test_api.xlsx", "Sheet1", case_id+1, 8, bb['final_re'])#调用写入excel函数
             baogao=bb['mm']+err
             print(baogao)
-            if "支付" in name:
+            if "支付" in name:    #如果接口名字有支付两个字
                 kk.append(name)
-                self.write_result("pay_api.xlsx", "Sheet1", case_id + 1, 8, bb['final_re'])
+                self.write_result("pay_api.xlsx", "Sheet1", case_id + 1, 8, bb['final_re'])  #写入支付接口excel文件
             kk.append(baogao)
 
-        return kk
-
-    # 支付功能
-
-    # 测试环境
-    def testpay(self,cases):
-    #     特惠商品支付
-        print(cases)
-        kk = []
-        for case in cases:
-            print(case)
-            case_id = case.get("case_id")  # 从列表中取出case_id
-            name=case.get('name')# 从列表中取出case_id
-            url = case.get("url")  # 从列表中取出url
-            method = case.get("method")  # 从列表中取出method
-            headers = eval(case.get("headers"))  # 从列表中取出headers
-            # return headers
-            data = eval(case.get("data"))  # 从列表中取出data注意取出来的是一个字符串，用eval（）函数可以去掉外面的双引号，直接用内部的值；
-            response = eval(case.get("response"))  # 从列表中取出response
-
-            msg = response.get("msg")  # 从response中取出msg
+        return kk   #返回内容
 
 
-            if method == "get":  # 判断请求方式
-                response = requests.get(url=url, headers=headers, data=data)
+    def  send_email(self):
+        # 链接邮箱服务器  password授权码
+        yag = yagmail.SMTP(user="1294424625@qq.com", password="dmaosvisrjzaibai", host='smtp.qq.com')
 
-            else:  # 这是post请求
-                response = requests.post(url=url, headers=headers, json=data,
-                                         verify=False)  # 调用请求方法，传入从列表取出的url，data,hedaers
-            bb = self.duanyan(response, msg, case_id, url)  # 调用断言方法返回通过结果列表
-            self.write_result("test_api.xlsx", "Sheet1", case_id + 1, 8, bb['final_re'])  # 调用写入excel函数
-            baogao = bb['mm']
-            kk.append(baogao)
+        # 邮箱正文
+        # contents = ['This is the body, and here is just text http://somedomain/image.png',
+        #             'You can find an audio file attached.', '/local/path/song.mp3']
 
-        return kk
+        contents = ['接口测试有请求失败接口']
 
-
-    # 线上支付测试
-    def mainpay(self,cases):
-
-        url = "https://yp.qhypay.cn/retail/user/wallet/pay_wallet_info"
-        headers = {"Content-Type": "application/json"}
-        data = {
-            "token": "MzowOGY5b1JKZVMvMGQ2Ym1MMEJNbnNjb1BseHpJWHl3UVlPOS9Idi9UYUpHeDJYWW83eW9KOjE3MDgzMzAwNzE=",
-            "version": "2.6.2",
-            "plat": "android",
-            "plat_ver": "QP1A.190711.020 release-keys",
-            "app_plat": "app",
-            "app_ver": "1.0.0.2",
-            "mobile_model": "V1838A",
-            "client": "app",
-            "device_id": "e56f17d1-0430-488b-ae20-9cab485621d7",
-            "getui_cid": "5394b115a1b684cb6a21fe394b19d430",
-            "deviceToken": "",
-            "timestamp": 1707034131,
-            "sign": "6c421c1e798f1e24218554f778195c6d"}
-        for case in cases:
-            print(case)
-   # 先获取账号余额和积分
+        # 发送邮件
+        yag.send('1294424625@qq.com', 'subject', contents)
 
 
-            case_id=case.get("case_id") #从列表中取出case_id
-            url=case.get("url")#从列表中取出url
-            method=case.get("method")#从列表中取出method
-            headers=eval(case.get("headers"))#从列表中取出headers
-            # return headers
-            data=eval(case.get("data"))#从列表中取出data注意取出来的是一个字符串，用eval（）函数可以去掉外面的双引号，直接用内部的值；
-            response=eval(case.get("response"))#从列表中取出response
-            msg = response.get("msg")  # 从response中取出msg
-            res=requests.post(url=url,json=data,headers=headers,verify=False).json()
-            bb = self.duanyan(res, msg, case_id, url)
+    # 定时任务任务函数具体方法
+
+    def task_func(self):
+        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
+
+        # return schedule.CancelJob
+        st.write('=============开始定时任务=============')
+        aa = apitest()
+        cases = aa.read_data("test_api.xlsx", "Sheet1")
+        # print(cases)
+        pp = aa.api_test(cases)
+        # st.text(pp)
+        for p in pp:
+            if "失败" in p:
+                st.text(p)
+                aa.send_email()
 
 
-    # 接口管理页面接口管理列表
-    # def apilist(self):
+    # 调用任务函数主方法
+    def task(self,name):
+        if name == 2:
 
-    # 下载文件
+            schedule.clear()
+        # schedule.every(50).seconds.do(self.task_func)
+        schedule.every().day.at("09:30").do(self.task_func)     #每天11点半执行定时任务
+
+
+        while True:
+            schedule.run_pending()
+
+
+
+
+
+
+
+
+    # def send_email(self):
+
+
 
 
 
@@ -320,28 +312,20 @@ def sidebar_bg(side_bg):
 # 调用
 # sidebar_bg('./999.jpg')
 
+
+
 def make_clickable(url):
     return f'<a target="_blank" href="{url}">www.baidu.com</a>'
 
 
-@st.cache
+
 def convert_df(df):
     # return df.to_csv("1.csv")
     return df.to_csv().encode('utf-8')
 
 
-
-
-
-# def get_binary_file_download_html(binfile,file_label='File'):
-#     with open(binfile,"rb")as f:
-#         data=f.read()
-#         binstr=base64.b64encode(data).decode()
-#         href=f'<a href="data:application/octet-stream,base64,{binstr}" download="{os.path.basename(binfile)}">dianwo{file_label}</a>'
-#
-#         return href
-
 # 页面具体自定义内容
+
 def foo():
     # st.title("请开始你的测试吧！！！")
 
@@ -369,9 +353,13 @@ def foo():
             st.text(p)
             if "失败" in p:
                 print(222)
-                st.markdown(":red[{p}]",p)
+                # st.markdown(":red[{p}]",p)
         st.write('==========测试结束=========')
         aa.testreport('pay')
+
+        # 定时任务按钮
+
+
 
 
 def bar():
@@ -411,19 +399,10 @@ def gen():
 
     st.table(pp)
 
-
-
-
-
-
     # name = st.text_input('请输入用户名', max_chars=100, help='最大长度为100字符')
-    #
-    # if st.button('搜索'):
-    #     st.write('搜索')
-    # # 根据用户输入进行操作
-    # st.write('您的用户名是', name)
 
 
+# 自定义接口管理页面调用方法
 
 def diy():
 
@@ -461,24 +440,27 @@ def diy():
     else:
         print(222)
 
+def time_task():
+    aa = apitest()
+    if st.button("停止定时任务"):
+        st.write("已经停止定时任务")
+        aa.task(name=2)
+
+    if st.button("开启定时任务(每天11点半执行定时任务)"):
+        aa.task(name=1)
+
+
+
+
+
+
 
 app = MultiApp()
 app.add_app("测试环境", foo)
 app.add_app("正式环境", bar)
 app.add_app("接口管理", gen)
 app.add_app("自定义接口数据测试", diy)
+app.add_app("定时任务", time_task)
 
 
 app.run()
-
-
-
-    # aa=apitest()
-    # cases=aa.read_data("test_api.xlsx","Sheet1")
-    # # print(cases)
-    # pp=aa.api_test(cases)
-    # st.text(pp)
-    # for p in pp:
-    #     st.text(p)
-    # st.write('==========测试结束=========')
-    # aa.testreport('test')
