8448433de1ef309204c3b93baa3ec105d3e17cbc
wengzx <1294424625@qq.com>
2024-10-02 20:12:05 +0800

    更新 yanzm.py

--- yzmshibie/yanzm.py
+++ yzmshibie/yanzm.py
@@ -1,43 +1,57 @@
-import ddddocr # 导入 ddddocr
-import requests
-import json
-import base64
+import cv2
+import numpy as np
+import streamlit as st
+import ddddocr
+from html5lib.html5parser import impliedTagToken
+
+import  requests
+from sympy.logic.algorithms.dpll import pl_true_int_repr
+
+# 设置标题
+st.title('在线验证码识别')
+
+# 上传图片并展示
+# uploaded_file = st.file_uploader("上传一张图片", type="png")
+
+# if uploaded_file is not None:
+#
+#     # 将传入的文件转为Opencv格式
+#     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+#     opencv_image = cv2.imdecode(file_bytes, 1)
+#     # 展示图片
+#     st.image(opencv_image, channels="BGR")
+#
+#     cv2.imwrite('test.png',opencv_image)
+# 然后就可以用这个图片进行一些操作了
 
 
-code_url = "https://www.sssgame.com/api/member/getCaptcha?type=login&timestamp=1727693541838"  # 验证码图片地址
-reponse = requests.get(url=code_url)  # 请求验证码
 
-#把响应数据json化为字典
-ss=json.loads(reponse.text)
+# 输入验证码地址
+img_code_url=st.text_input("验证码地址")
 
-# 获取验证码图片地址
-img_code_url1=ss['data']["codeUrl"]
-print(img_code_url1)
+# 将request 开启会话
+session = requests.session()
 
-try:
-    # 截取uri的data:image/png;base64后端的uri
-    img_code_url=img_code_url1.split(",")[1]
-    # 这个就是解析
-    ss=base64.b64decode(img_code_url)
-    print(ss)
+if "https://" in img_code_url  and img_code_url is not None:
 
-except Exception as e:
-        print(f"base64_to_img error:{e}")
+    # 获取验证码图片地址
+    png = session.get(url=img_code_url)  # 请求验证码
+    print(png)
 
-
-# 将验证码保存本地
-filename = "a8.png"  # 定义一个图片地址
-file = open(filename, "wb")  # 以二进制打开一个文件
-file.write(ss)  # 写入二进制文件
-file.close()  # 关闭文件
+    # 将验证码保存本地
+    filename = "a8.png"  # 定义一个图片地址
+    file = open(filename, "wb")  # 以二进制打开一个文件
+    file.write(png.content)  # 写入二进制文件
+    file.close()  # 关闭文件
 
 
 ocr = ddddocr.DdddOcr(show_ad=False) # 实例化
 
-with open('a8.png', 'rb') as f: # 打开图片
+with open('test.png', 'rb') as f: # 打开图片
 
     img_bytes = f.read() # 读取图片
     res = ocr.classification(img_bytes) # 识别
 
     # 打印验证码
     print(res)
+    st.text("验证码识别结果为："+res)
